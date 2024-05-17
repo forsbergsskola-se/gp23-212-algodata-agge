724a590bfcd9f5e349899a4020f464baeb62b86d
augustlewen <augustlewen@live.com>
2024-05-17 15:41:14 +0200

    added tests for count, remove and remove at

--- Algorithms-And-DataStructures/TurboCollections.Test/TurboLinkedListTest.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboLinkedListTest.cs
@@ -13,4 +13,43 @@ public class TurboLinkedListTest
 
         CollectionAssert.AreEqual(new []{1, 2, 3}, list);
     }
+    
+    [Test]
+    public void TestRemove()
+    {
+        var list = new TurboLinkedList<int>();
+        
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Remove(2);
+
+        CollectionAssert.AreEqual(new []{1, 3}, list);
+    }
+    
+    [Test]
+    public void TestRemoveAt()
+    {
+        var list = new TurboLinkedList<int>();
+        
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.RemoveAt(1);
+
+        CollectionAssert.AreEqual(new []{1, 3}, list);
+    }
+    
+    [Test]
+    public void TestCount()
+    {
+        var list = new TurboLinkedList<int>();
+        
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+
+        Assert.That(list.Count(), Is.EqualTo(4));
+    }
 }
