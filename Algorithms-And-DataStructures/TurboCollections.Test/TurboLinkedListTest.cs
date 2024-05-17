50c285c8d12a21c5d03075841d8428a26034828e
augustlewen <augustlewen@live.com>
2024-05-17 16:01:44 +0200

    added clear test

--- Algorithms-And-DataStructures/TurboCollections.Test/TurboLinkedListTest.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboLinkedListTest.cs
@@ -2,6 +2,7 @@ namespace TurboCollections.Test;
 
 public class TurboLinkedListTest
 {
+
     [Test]
     public void TestAdd()
     {
@@ -41,6 +42,32 @@ public class TurboLinkedListTest
     }
     
     [Test]
+    public void TestClear()
+    {
+        var list = new TurboLinkedList<int>();
+        
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        
+        Assert.That(list, Is.Not.Empty);
+        list.Clear();
+        Assert.That(list, Is.Empty);
+    }
+    
+    [Test]
+    public void TestPeek()
+    {
+        var list = new TurboLinkedList<int>();
+        
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        
+        Assert.That(list.Peek(), Is.EqualTo(1));
+    }
+    
+    [Test]
     public void TestCount()
     {
         var list = new TurboLinkedList<int>();
@@ -50,6 +77,6 @@ public class TurboLinkedListTest
         list.Add(3);
         list.Add(4);
 
-        Assert.That(list.Count(), Is.EqualTo(4));
+        Assert.That(list.Count, Is.EqualTo(4));
     }
 }
