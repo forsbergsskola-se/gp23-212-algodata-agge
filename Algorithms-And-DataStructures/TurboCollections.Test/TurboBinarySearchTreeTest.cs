7c79a90ff0f6adc896ea9ffd18c722cae456738b
augustlewen <augustlewen@live.com>
2024-05-20 14:34:10 +0200

    added  insert test

--- Algorithms-And-DataStructures/TurboCollections.Test/TurboBinarySearchTreeTest.cs
+++ Algorithms-And-DataStructures/TurboCollections.Test/TurboBinarySearchTreeTest.cs
@@ -2,5 +2,18 @@ namespace TurboCollections.Test;
 
 public class TurboBinarySearchTreeTest
 {
-    
+    [Test]
+    public void InsertTest()
+    {
+        var tree = new TurboBinarySearchTree<int>();
+
+        tree.Insert(5);
+        tree.Insert(2);
+        tree.Insert(4);
+        tree.Insert(8);
+        tree.Insert(3);
+        
+        Assert.Pass();
+    }
+        
 }
